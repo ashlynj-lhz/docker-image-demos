5bd658c6c07fb4985bd3cf110ee49a32dd5cfc04
Ajeet Singh Raina, Docker Captain, ARM Innovator <ajeetraina@gmail.com>
2024-01-16 16:03:42 +0530

    Create index.js

---
+++ node/index.js
@@ -0,0 +1,16 @@
+// index.js
+
+// Import required modules
+const http = require('http');
+
+// Create a simple HTTP server that responds with "Hello, Docker!" for all requests
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, Docker!\n');
+});
+
+// Set the server to listen on port 3000
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
